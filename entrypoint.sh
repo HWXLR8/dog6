5317fc03a0656aa14a6fc2f72b3c2135ba6b13aa
HWXLR8 <68k@segv.moe>
2026-08-01 12:28:22 -0400

    Add cert renewal

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,18 @@
 
 set -e
 
+renew_certs_forever() {
+    while true; do
+        sleep 12h
+        echo "checking SSL cert renewal..."
+        certbot renew \
+            --webroot --webroot-path=/srv/http \
+            --quiet \
+            --deploy-hook "cat /etc/letsencrypt/live/dog6.net/privkey.pem /etc/letsencrypt/live/dog6.net/fullchain.pem > /etc/letsencrypt/live/dog6.net/lighttpd.pem && pkill -HUP lighttpd" \
+            || echo "warning: certbot renew failed"
+    done
+}
+
 # start lighttpd in the background so certbot can use HTTP-01
 echo "starting lighttpd..."
 lighttpd -f /etc/lighttpd/lighttpd.conf
@@ -15,5 +27,7 @@ bash /certbot-init.sh
 echo "killing lighttpd..."
 pkill lighttpd
 sleep 1
+echo "starting cert renewal loop..."
+renew_certs_forever &
 echo "starting lighttpd..."
 exec lighttpd -D -f /etc/lighttpd/lighttpd.conf
